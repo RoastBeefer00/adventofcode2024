5ab176b6260c41d96629c11c1969ce7bcbd926e7
Jake Jasmin <roastbeefer000@gmail.com>
2024-12-27 10:28:05 -0700

    day7 part1 done

--- day7/src/bin/part1.rs
+++ day7/src/bin/part1.rs
@@ -1,27 +1,27 @@
-use anyhow::{Result, anyhow};
 use std::fs::read_to_string;
+use anyhow::Result;
 use nom::{
-    bytes::complete::tag, character::complete::{anychar, char, i32, line_ending, one_of, space0}, combinator::opt, multi::many1, sequence::{preceded, terminated, tuple}, IResult
+    bytes::complete::tag, character::complete::{i64, line_ending}, multi::many1, sequence::{preceded, terminated, tuple}, IResult
 };
 use itertools::Itertools;
 
-fn process_line(input: &str) -> IResult<&str, (i32, Vec<i32>)> {
+fn process_line(input: &str) -> IResult<&str, (i64, Vec<i64>)> {
     tuple((
-        terminated(i32, tag(":")), 
+        terminated(i64, tag(":")), 
         many1(
             preceded(
                 tag(" "), 
-                i32
+                i64
             )
         )
     ))(input)
 }
 
-fn get_all_lines(input: &str) -> IResult<&str, Vec<(i32, Vec<i32>)>> {
+fn get_all_lines(input: &str) -> IResult<&str, Vec<(i64, Vec<i64>)>> {
     many1(terminated(process_line, line_ending))(input)
 }
 
-fn is_valid_combination(result: &i32, nums: &Vec<i32>, combination: Vec<char>) -> bool {
+fn is_valid_combination(result: &i64, nums: &Vec<i64>, combination: Vec<char>) -> bool {
     assert_eq!(nums.len() - 1, combination.len());
     let mut total = 0;
     combination.
@@ -60,19 +60,41 @@ fn is_valid_combination(result: &i32, nums: &Vec<i32>, combination: Vec<char>) -
 }
 
 fn main() -> Result<()> {
+    let input = read_to_string("input.txt")?;
+    let (_, lines) = get_all_lines(&input).unwrap();
+    let sum: i64 = lines
+        .into_iter()
+        .filter(|(result, nums)| {
+            let mut operators = Vec::new();
+            for _ in 0..nums.len() - 1 {
+                operators.push('+');
+                operators.push('*');
+            }
+            let combinations = operators.into_iter().combinations(nums.len() - 1).collect::<Vec<_>>();
+            let count = combinations.into_iter().filter(|comb| {
+                is_valid_combination(&result, &nums, comb.clone())
+            }).count();
+
+            count > 0
+        })
+        .map(|(result, _)| result)
+        .sum();
+    println!("{sum}");
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
+    use std::fs::read_to_string;
+
     use super::*;
 
     #[test]
     fn test_example() -> Result<()> {
         let input = read_to_string("example.txt")?;
         let (_, lines) = get_all_lines(&input).unwrap();
-        let sum: i32 = lines
+        let sum: i64 = lines
             .into_iter()
             .filter(|(result, nums)| {
                 let mut operators = Vec::new();
